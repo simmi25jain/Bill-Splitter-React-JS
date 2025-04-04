9d3692d2f947157e0a409dee9ce2c3d33f73478c
simmi25jain <simmi5jain4@gmail.com>
2025-04-04 17:17:46 +0530

    change frontend path

--- back/index.js
+++ back/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
   // origin: process.env.FRONTEND_URL,
-  origin: `https://bill-splitter-react-js-front.onrender.com`,
+  origin: `https://bill-splitter-react-js-1.onrender.com`,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
