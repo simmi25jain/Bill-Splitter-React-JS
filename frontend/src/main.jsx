46eb7de9784e839e6c3c943980ae11dc819a3789
simmi25jain <simmi5jain4@gmail.com>
2025-04-04 16:52:59 +0530

    change axios base url

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,9 @@
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
+// import Bill from './Bill.jsx'
 
 createRoot(document.getElementById('root')).render(
 <App/>
+// {/* <Bill/> */}
 )
